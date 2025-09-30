620ee34291b9b56e501f78e65a11d412f489034a
jannevaril <jan.nevaril@tietoevry.com>
2025-09-30 10:35:26 +0200

    test mq dep

--- app.tf
+++ app.tf
@@ -1,6 +1,6 @@
 module "module-monitoring-mq" {
   source  = "app.terraform.io/jan-nevaril-dev/mq/aws"
-  version = "0.0.1"
+  version = "0.0.2"
 
   # General Configuration
   customer                            = "jannevaril"#var.default-tags.Customer
